240d7a9fb4e32a47183a6f48456bbb5697b01900
Luan Lima <luan@sippulse.com>
2018-11-09 19:11:40 -0200

    Implementação das chamadas para os endpoints Getll e Search

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
-      
-    render() {
 
+
+
+    render() {
         const { books } = this.props;
 
         return (
             <div>
-                {books.map(book => (
-                    <li key={book.title}>
+                {(books !== undefined && !books.items) && books.map(book => (
+                    <li key={book.id}>
                         <div className="book">
                             <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                 <div className="book-shelf-changer">
                                     <select>
                                         <option value="move" disabled>Move to...</option>
--- src/components/booksearch/BookSearch.js
+++ src/components/booksearch/BookSearch.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import * as BooksAPI from '../../BooksAPI';
+import Book from '../book/Book'
 
 class BookSearch extends Component {
 
+    state = {
+        query: '',
+        books: []
+    }
+
+    update = (query) => {
+        this.updateQuery(query);
+        this.updaetBooks(query);
+    }
+
+    updaetBooks(query) {
+        if (query) {
+            BooksAPI.search(query)
+                .then((books) => this.setState(() => ({ books })))
+        }
+    }
+
+    updateQuery = (query) => {
+        console.log(query);
+
+        this.setState(() => ({
+            query: query.trim()
+        }));
+    }
+
     render() {
+
+        const { query, books } = this.state;
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link className="close-search" to="/">Close</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author" />
-
+                        <input type="text" placeholder="Search by title or author" value={query} onChange={(event) => this.update(event.target.value)} />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        <Book books={books} />
+                    </ol>
                 </div>
             </div>
         );
